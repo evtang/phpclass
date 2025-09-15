eda1d13197e93cd8f9ebccb76192cf7f661c1b8d
Eddie Tang <eddietang001@gmail.com>
2025-09-15 18:41:16 -0500

    first commit

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>Eddie's website</h1>
+</header>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eddie's website</title>
+    <link rel="stylesheet" href="/css/base.css">
+</head>
+<body>
+<?php
+    include "includes/header.php";
+?>
+    <div id="three-column">
+<?php
+    include "includes/navigation.php";
+?>
+        <main>
+            <img src="images/eddie.jpg"
+            <p>On my honeymoon with my wife in Vietnam!</p>
+        </main>
+    </div>
+<?php
+    include "includes/footer.php";
+?>
+</body>
+</html>
