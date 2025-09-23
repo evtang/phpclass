fb07f107ab5e9169fc8bf8ec0d05d82814333d01
Eddie Tang <eddietang001@gmail.com>
2025-09-22 19:12:07 -0500

    finished 8 ball

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,14 +1,15 @@
 <?php
 
-$isHome = $_SERVER['REQUEST_URI'] == '/';
-$isLoops = $_SERVER['REQUEST_URI'] == '/loops';
-$isCountdown = $_SERVER['REQUEST_URI'] == '/countdown';
-
+$isHome = $_SERVER['REQUEST_URI'] == '/' ? 'selected' : '';
+$isLoops = $_SERVER['REQUEST_URI'] == '/loops' ? 'selected' : '';
+$isCountdown = $_SERVER['REQUEST_URI'] == '/countdown' ? 'selected' : '';
+$isMagic8Ball = $_SERVER['REQUEST_URI'] == '/magic-8ball' ? 'selected' : '';
 ?><nav>
     <?=$_SERVER['REQUEST_URI']?>
     <ul>
         <li class="<?=$isHome?>"><a href="/">Home</a></li>
         <li class="<?=$isLoops?>"><a href="/loops">Loops</a></li>
         <li class="<?=$isCountdown?>"><a href="/countdown">Countdown</a></li>
+        <li class="<?=$isMagic8Ball?>"><a href="/magic-8ball">Magic 8 Ball</a></li>
     </ul>
 </nav>
